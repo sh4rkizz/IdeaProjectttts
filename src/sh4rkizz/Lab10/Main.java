25c4917ffb12f592a7b9bd6aec4cf886ec2ba6ae
sh4rkizz <70894081+sh4rkizz@users.noreply.github.com>
2020-10-11 01:39:06 +0300

    Лаб_10

---
+++ src/sh4rkizz/Lab10/Main.java
@@ -0,0 +1,10 @@
+package sh4rkizz.Lab10;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator("Calculator");
+        calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
