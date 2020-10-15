6f60885a0517743abbc8c539579c3eab17ebadf9
sh4rkizz <70894081+sh4rkizz@users.noreply.github.com>
2020-10-15 23:09:53 +0300

    Лаб_12

---
+++ src/sh4rkizz/Lab12/ColoredText.java
@@ -0,0 +1,28 @@
+package sh4rkizz.Lab12;
+
+public enum ColoredText {
+    RED_TEXT("\u001B[31m", "Red"),
+    GREEN_TEXT("\u001B[32m", "Green"),
+    YELLOW_TEXT("\u001B[33m", "Yellow"),
+    BLUE_TEXT("\u001B[34m", "Blue"),
+    PURPLE_TEXT("\u001B[35m", "Purple"),
+    CYAN_TEXT("\u001B[36m", "Cyan"),
+    WHITE_TEXT("\u001B[30m", "White"),
+    RESET_COLOR("\u001B[0m", "");
+
+    private final String color;
+    private final String name;
+
+    ColoredText(String color, String name) {
+        this.color = color;
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
