6a5fda6fab22c975fbd2f6bb0573cd75056de42d
sh4rkizz <70894081+sh4rkizz@users.noreply.github.com>
2020-10-18 15:32:29 +0300

    Лаб_11

--- src/sh4rkizz/Lab11/ThreadsOperation.java
+++ src/sh4rkizz/Lab11/ThreadsOperation.java
@@ -1,21 +1,30 @@
 package sh4rkizz.Lab11;
 
-import java.util.ArrayList;
-
 public class ThreadsOperation {
     public static void main(String[] args) throws InterruptedException {
         long startTime = System.currentTimeMillis();
-        ArrayList<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
             Thread thread = new Threads();
             thread.start();
-            threads.add(thread);
             thread.join();
         }
 
         long endTime = System.currentTimeMillis();
+        long result = endTime - startTime;
+        System.out.println("Total threads time: " + result);
+
+        startTime = System.currentTimeMillis();
+        Threads main = new Threads();
+
+        for (int i = 0; i < 10; i++)
+            main.run();
+
+        endTime = System.currentTimeMillis();
         System.out.println("Total runs: " + Threads.getCountRuns());
-        System.out.println("Total time: " + (endTime - startTime));
+        System.out.println("Total main thread time: " + (endTime - startTime));
+
+        result -= endTime - startTime;
+        System.out.println("The difference between timers equals: " + result);
     }
 }
