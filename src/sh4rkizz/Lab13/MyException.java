0066f435480b42d17df609518b0434cf1390b77a
sh4rkizz <70894081+sh4rkizz@users.noreply.github.com>
2020-10-19 23:21:59 +0300

    Лаб_13

---
+++ src/sh4rkizz/Lab13/MyException.java
@@ -0,0 +1,4 @@
+package sh4rkizz.Lab13;
+
+public class MyException extends IllegalAccessException {
+}
---
+++ src/sh4rkizz/Lab13/MyRuntimeException.java
@@ -0,0 +1,7 @@
+package sh4rkizz.Lab13;
+
+public class MyRuntimeException extends RuntimeException {
+    public MyRuntimeException() {
+        System.out.println("Сработал 'MyRuntimeException'");
+    }
+}
